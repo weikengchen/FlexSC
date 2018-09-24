88335afcacde66aba3311861992f40c597696dd0
Weikeng Chen <weikeng@eecs.berkeley.edu>
2018-09-24 08:37:32 +0000

    update test file

--- test/oram/TestCircuitOramRecClient.java
+++ test/oram/TestCircuitOramRecClient.java
@@ -7,9 +7,9 @@ import flexsc.Flag;
 public class TestCircuitOramRecClient {
 
 	public  static void main(String args[]) throws Exception {
-		for(int i = 12; i <=20 ; i+=2) {
+		for(int i = 12; i <=16 ; i+=4) {
 			Flag.sw.flush();
-			EvaRunnable eva = new EvaRunnable("52.9.51.202", 6550);
+			EvaRunnable eva = new EvaRunnable("172.27.234.1", 6550);
 			eva.run();
 			Flag.sw.print();
 			System.out.print("\n");
--- test/oram/TestCircuitOramRecServer.java
+++ test/oram/TestCircuitOramRecServer.java
@@ -6,10 +6,10 @@ import flexsc.Flag;
 public class TestCircuitOramRecServer {
 
 	public  static void main(String args[]) throws Exception {
-		for(int i = 12; i <=20 ; i+=2) {
+		for(int i = 12; i <= 16; i+=4) {
 			Flag.sw.flush();
 			Flag.countIO = true;
-			GenRunnable gen = new GenRunnable(6550, i, 3, 65536, 8, 6);
+			GenRunnable gen = new GenRunnable(6550, i, 3, 4096, 8, 6);
 			gen.run();
 			Flag.sw.print();
 			System.out.print("\n");
